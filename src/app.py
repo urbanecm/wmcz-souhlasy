3c9506982fc9ced6a9c3fa20a59ff026a8a52872
Martin Urbanec <martin@urbanec.cz>
2019-04-11 16:18:30 +0200

    Update

---
+++ src/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import yaml
+
+app = Flask(__name__)
+
+# Load configuration from YAML file
+__dir__ = os.path.dirname(__file__)
+app.config.update(
+    yaml.safe_load(open(os.path.join(__dir__, os.environ.get(
+        'FLASK_CONFIG_FILE', 'config.yaml')))))
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+if __name__ == "__main__":
+	app.run(debug=True, port=2000)
